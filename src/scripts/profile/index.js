a296ccabafefc1e09c8c79d348ece14667195e76
ghspbravo <gmspbravo@gmail.com>
2020-09-17 22:41:16 +0300

    feat: add profile badge in profile for buddies

--- src/scripts/profile/index.js
+++ src/scripts/profile/index.js
@@ -1,3 +1,4 @@
+import Tooltip from 'bootstrap/js/src/tooltip';
 import { getCurrentUserId } from '../auth';
 import {
   formDataToObj,
@@ -164,6 +165,14 @@ function drawUserProfile() {
       reader.readAsDataURL(this.files[0]);
     }
   });
+  const buddyBadgeNode = document.querySelector('[data-toggle="tooltip"]');
+  if (buddyBadgeNode) {
+    new Tooltip(buddyBadgeNode);
+  }
+  // TODO: define common isNotConfirmedBuddy()
+  if (!currentUser.isBuddy && currentUser.place) {
+    document.getElementById('profile-moderation').classList.remove('d-none');
+  }
 }
 
 function drawUserEditProfile() {
